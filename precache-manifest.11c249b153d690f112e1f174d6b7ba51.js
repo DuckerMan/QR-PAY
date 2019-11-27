82fea60917fe5587c760744b3b7eaf68fd91b63f
Sergey Duck <sergeypechnikov326@gmail.com>
2019-11-27 11:06:18 +0300

    Updates

---
+++ precache-manifest.11c249b153d690f112e1f174d6b7ba51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c82baab55d6bf18bb3c052dc44f4f8",
+    "url": "/QR-PAY/index.html"
+  },
+  {
+    "revision": "f23cb1eea7d28c543238",
+    "url": "/QR-PAY/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "19d356ecf6ea6483abe6",
+    "url": "/QR-PAY/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "f23cb1eea7d28c543238",
+    "url": "/QR-PAY/static/js/2.512243e3.chunk.js"
+  },
+  {
+    "revision": "19d356ecf6ea6483abe6",
+    "url": "/QR-PAY/static/js/main.0c3c04b5.chunk.js"
+  },
+  {
+    "revision": "1ce29351ba3a7da2a0e1",
+    "url": "/QR-PAY/static/js/runtime-main.783cd9d8.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/QR-PAY/static/media/persik.4e1ec840.png"
+  }
+]);
